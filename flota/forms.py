caf92a6e03890b3f6673887dd7a24c437ba77d5e
Felipe Vásquez <felvasquez93@gmail.com>
2026-05-28 15:33:09 -0400

    Corrige import faltante para validacion de RUT

--- flota/forms.py
+++ flota/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from .models import Vehiculo, CentroCosto, MantencionVehiculo
-
+import re
 
 # ============================================================
 # Formulario de vehículos
